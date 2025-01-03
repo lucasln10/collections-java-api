99c314afc9b66e7d4af278dee5a6c2165abda47d
Hershell Lucas <hershellsousa@gmail.com>
2025-01-03 12:59:09 -0300

    ajuste na tarefa de collections

--- collectionsJAVAapi/src/collectionsJAVAapi/CarrinhoDeCompras.java
+++ collectionsJAVAapi/src/collectionsJAVAapi/CarrinhoDeCompras.java
@@ -14,20 +14,32 @@ public class CarrinhoDeCompras {
 		this.listaItens.add(item);
 	}
 	@SuppressWarnings("unlikely-arg-type")
-	public void removerItem(String nome) { 
+	public void removerItem(String nome) {
 		this.listaItens.remove(nome);
+
 	}
 	public void calcularValorTotal() {
-		double total = 0.0;
-		
+		double total = 0.0; 
 		for (Item item : listaItens) {
-			total  = item.getPreco() * item.getQuantidade();
+			total += item.getPreco() * item.getQuantidade();
 		}
+		System.out.println(total);
 	}
 	public void exibirItens() {
-		for(Item i : listaItens) {
+		System.out.println("os itens comprados foram: " + listaItens);
+		/*for(Item i : listaItens) {
 			System.out.println(i);
-		}
+		}*/
+	}
+	public static void main(String[] args) {
+		CarrinhoDeCompras compras = new CarrinhoDeCompras();
+		compras.adicionarItem("nescau", 10.98, 2);
+		compras.adicionarItem("arroz", 32.85, 3);
+		compras.adicionarItem("feijao", 30.85, 3);
+		
+		compras.exibirItens();
+		
+		
 	}
 	
 }
