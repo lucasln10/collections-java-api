8d9837b92f4175307aa62ae62526b4992d1042a1
Hershell Lucas <hershellsousa@gmail.com>
2025-01-08 23:29:29 -0300

    Gerenciador de alunos

---
+++ collectionsJAVAapi/src/alunos/Alunos.java
@@ -0,0 +1,33 @@
+package alunos;
+
+public class Alunos {
+	private String nome;
+	private long matricula;
+	private double nota;
+	
+	public Alunos (String nome, long matricula, double nota) {
+		this.nome = nome;
+		this.matricula = matricula;
+		this.nota = nota;
+		
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public long getMatricula() {
+		return matricula;
+	}
+	
+	public double getNota() {
+		return nota;
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "aluno: " + nome + ". matricula: " + matricula + ". nota:" + nota ;
+	}
+}
---
+++ collectionsJAVAapi/src/alunos/GerenciadorAlunos.java
@@ -0,0 +1,90 @@
+package alunos;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class GerenciadorAlunos {
+	Set<Alunos> escola;
+	
+	public GerenciadorAlunos () {
+		this.escola = new HashSet<>();
+		
+	}
+	
+	public void adicionarAluno(String nome, long matricula, double media) {
+		Alunos alunoExistente = buscarAlunoPorMatricula(matricula);
+		if (alunoExistente != null) {
+			System.out.println("esta matricula " + matricula + " já existe");			
+		}
+		else if (media < 8) {
+			System.out.println("sua nota não atinge os padrões exigidos para está escola");
+		}
+		else {
+			escola.add(new Alunos(nome, matricula, media));
+		}
+	}
+	
+	
+	public void removerAluno(long matricula) {
+		Alunos verificarAluno = buscarAlunoPorMatricula(matricula);
+		Alunos removerAluno = null;
+			if (verificarAluno != null) {
+				removerAluno = verificarAluno;
+			}
+			escola.remove(removerAluno);
+		}
+	
+	
+	public void exibirAlunosPorNome() {
+		for (Alunos a : escola) {
+			String nomes = a.getNome();
+			System.out.println("nome do aluno " + nomes);
+		}
+	}
+	
+	
+	public void exibirAlunosPorNota() {
+		for (Alunos a : escola) {
+		 int notas = (int) a.getNota();
+			System.out.println("nota do aluno " + notas);
+		}
+	}
+	
+	
+	public void exibirAlunos() {
+		System.out.println(escola);
+	}
+	
+	
+	//metodo para verificar se a matricula já exite
+	private Alunos buscarAlunoPorMatricula(long matricula) {
+		for (Alunos aluno : escola) {
+		    if (aluno.getMatricula() == matricula) {
+		       return aluno;
+		    }
+		}
+		return null; // Retorna null se não encontrar o aluno
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		GerenciadorAlunos aluno = new GerenciadorAlunos();
+		
+		aluno.adicionarAluno("Davi", 12345, 8);
+		aluno.adicionarAluno("julio", 123456, 7);
+		aluno.adicionarAluno("Davi", 12345, 8);
+		aluno.adicionarAluno("manuela", 1234567, 9);
+		
+		aluno.exibirAlunos();
+		
+		aluno.removerAluno(1234567);
+		
+		aluno.exibirAlunos();
+		
+		aluno.exibirAlunosPorNota();
+		aluno.exibirAlunosPorNome();
+		
+	}
+	
+}
